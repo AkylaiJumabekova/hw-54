10c7ece7fffa1c35ed966448cbdd5b8f6525cfea
Zhumabekova Akylai <jumabekovaakylai7@gmail.com>
2024-05-28 17:26:19 +0600

    fixed error

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import GameBoard from "../../components/GameBoard/GameBoard";
 import AttemptsCounter from "../../components/AttemptsCounter/AttemptsCounter";
 import ResetButton from "../../components/ResetButton/ResetButton";
